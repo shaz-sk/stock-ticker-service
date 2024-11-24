b0e453fa0481715d725ded33315a27942f4dc8d2
shaz-sk <shanka900@gmail.com>
2024-11-24 14:16:54 +1100

    initial-commit

--- src/test/kotlin/com/organization/stockTicker/mapper/StockDetailsMapperTest.kt
+++ src/test/kotlin/com/organization/stockTicker/mapper/StockDetailsMapperTest.kt
@@ -1,5 +1,26 @@
 package com.organization.stockTicker.mapper
 
+import com.organization.stockTicker.TestDataProvider.Companion.timeSeriesMappedData
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.junit.jupiter.MockitoExtension
+import kotlin.test.BeforeTest
 
-class StockDetailsMapperTest
+@ExtendWith(MockitoExtension::class)
+class StockDetailsMapperTest {
+    private lateinit var stockDetailsMapper: StockDetailsMapper
+    private val days = 3
+
+    @BeforeTest
+    fun setup() {
+        stockDetailsMapper = StockDetailsMapper(days)
+    }
+    @Test
+    fun `should map from StockData to StockDetails`() {
+        val stockDetails = stockDetailsMapper.mapToStockDetails(timeSeriesMappedData)
+        assertEquals(timeSeriesMappedData.metaData.symbol, stockDetails.symbol)
+        assertEquals(days, stockDetails.averagePeriod)
+        //TODO
+    }
+}
